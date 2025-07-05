b969a635dc19dccf73c85e6503794612771f75e7
shoaibahmed230 <shoaibahmed230@gmail.com>
2025-07-05 08:42:39 +0500

    added confiq

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = {
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'dso02dmas',
+        api_key: '518387236662156',
+        api_secret: 'IeWh-PAcAMy2BLvvuLJWuPZTo7Q',
+      },
+    },
+  },
+};
